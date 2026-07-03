8bce2564e5323ebbcb3ebd365cac01a7b75fe098
anagit2022 <107537622+anagit2022@users.noreply.github.com>
2026-07-03 16:37:19 +0800

    Add fetch API to send data on button click

--- sketch.js
+++ sketch.js
@@ -1,1 +1,21 @@
+const scriptURL = "YOUR_SCRIPT_URL";
 
+document.getElementById("sendButton").onclick = function () {
+
+    const data = {
+        message: "Hello from GitHub!"
+    };
+
+    fetch(scriptURL, {
+        method: "POST",
+        body: JSON.stringify(data)
+    })
+    .then(response => response.text())
+    .then(result => {
+        alert(result);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+};
